117023f81beb4cf59f175324c2d211427c7530ef
kap1bala <243586394+kap1bala@users.noreply.github.com>
2026-01-16 13:20:19 +0800

    feat: add main feat

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,12 @@
  * 如需了解更多开发细节，请阅读：
  * https://prodocs.lceda.cn/cn/api/guide/
  */
-import * as extensionConfig from '../extension.json';
+
+// import * as extensionConfig from '../extension.json';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function activate(status?: 'onStartupFinished', arg?: string): void {}
 
-export function about(): void {
-	eda.sys_Dialog.showInformationMessage(
-		eda.sys_I18n.text('EasyEDA extension SDK v', undefined, undefined, extensionConfig.version),
-		eda.sys_I18n.text('About'),
-	);
+export function openHowBigIsItIFrame(): void {
+	eda.sys_IFrame.openIFrame(`/iframe/index.html`, 500, 800, 'howBigIsIt');
 }
